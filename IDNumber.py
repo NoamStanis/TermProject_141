7b4d37f98e4d0da9e09c0adb84e201e2250ffd15
Noam <NoamStanis@users.noreply.github.com>
2019-12-08 18:02:36 -0500

    Vertical + Horizontal working together without multiplier

--- IDNumber.py
+++ IDNumber.py
@@ -91,32 +91,22 @@ class TribeBubbles(QWidget):
 
             # Check for scoring
 
-            for row in self.circles:  # Checks horizontal scoring
-                rowstring = ''.join(row)
+            vertcircles = list(map(list, zip(*self.circles)))
+            for row in range(len(self.circles)):  # Checks horizontal scoring
+                rowstring = ''.join(self.circles[row])
                 nO = 8
-                while 4 <= nO <= 8:
+                while nO >= 4:
                     if 'O' * nO in rowstring:
-                        for i in range(len(row)):
-                            try:
-                                if row[i:i + nO] == ['O' for j in range(nO)] and not isinstance(
-                                        self.blockers[self.circles.index(row)][i], QRect):
-                                    row[i:i + nO] = ['_' for k in range(nO)]
-                                    self.score += nO
-                            except IndexError:
-                                continue
+                        first = rowstring.index('O')
+                        last = rowstring.rindex('O')
+                        for j in range(first, last + 1):
+                            self.circles[row][j] = '_'
+                        self.score += nO
+                        break
                     if nO == 4:
                         break
                     else:
                         nO -= 1
-
-            vertcircles = list(map(list, zip(*self.circles)))
-
-            print("\ncircles")
-            for row in self.circles:
-                print(row)
-            print("\ninverted circles")
-            for c in vertcircles:
-                print(c)
 
             for col in range(len(vertcircles)):  # vertical score check
                 col_string = ''.join(vertcircles[col])
@@ -125,7 +115,7 @@ class TribeBubbles(QWidget):
                     if 'O' * numberOs in col_string:
                         first = col_string.index('O')
                         last = col_string.rindex('O')
-                        for i in range(first, last+1):
+                        for i in range(first, last + 1):
                             self.circles[i][col] = '_'
                         self.score += numberOs
                         break
